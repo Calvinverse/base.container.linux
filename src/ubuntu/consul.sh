343cefde5de2700261ecc5f365dd38f27604c25f
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-07-20 18:48:32 +1200

    Updating the build scripts to have a test docker container

--- src/ubuntu/consul.sh
+++ src/ubuntu/consul.sh
@@ -64,20 +64,17 @@ if [ -n "$CONSUL_ENCRYPT" ]; then
   echo "==> setting encrypt option..."
 fi
 
+if [ -n "$CONSUL_DOMAIN"]; then
+  echo "server=/$CONSUL_DOMAIN/127.0.0.1#8600" > /etc/dnsmasq.d/10-consul
+  echo "==> creating dnsmasq consul redirect..."
+fi
+
+
 # CONSUL_DATA_DIR is exposed as a volume for possible persistent storage. The
 # CONSUL_CONFIG_DIR isn't exposed as a volume but you can compose additional
 # config files in there if you use this image as a base
 CONSUL_DATA_DIR=/etc/consul/data
 CONSUL_CONFIG_DIR=/etc/consul/conf.d
-
-# If the data or config dirs are bind mounted then chown them.
-# Note: This checks for root ownership as that's the most common case.
-# if [ "$(stat -c %u /consul/data)" != "$(id -u consul)" ]; then
-#   chown consul:consul /consul/data
-# fi
-# if [ "$(stat -c %u /consul/config)" != "$(id -u consul)" ]; then
-#   chown consul:consul /consul/config
-# fi
 
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
