1aec4dc933d69067ffbd14d2b3b0b313be2b65a3
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-10-21 15:30:42 +1300

    Fixing issues with the unbound service
    references #2

--- src/unbound.sh
+++ src/unbound.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-CONSUL_DOMAIN=
 if [ -n "$CONSUL_DOMAIN_NAME" ]; then
   echo "server:" > /etc/unbound.d/unbound_zones.conf
   echo "  local-zone: \"$CONSUL_DOMAIN_NAME.\" nodefault" >> /etc/unbound.d/unbound_zones.conf
@@ -19,4 +18,4 @@ fi
 
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec /usr/sbin/unbound agent -d -c /etc/unbound/unbound.conf 2>&1 | logger
+exec /usr/sbin/unbound -d -c /etc/unbound/unbound.conf 2>&1 | logger
